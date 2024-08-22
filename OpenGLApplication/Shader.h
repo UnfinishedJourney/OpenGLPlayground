2d5159e47eb59ec382753c723599e7d872a30f04
Olga Sabirova <osabirova94@gmail.com>
2024-08-22 19:14:35 +0200

    17. Textures in OpenGL

--- OpenGLApplication/Shader.h
+++ OpenGLApplication/Shader.h
@@ -13,7 +13,7 @@ class Shader
 private:
 	std::string m_FilePath;
 	unsigned int m_RendererID;
-	std::unordered_map<std::string, unsigned int> m_UniformLocationCache;
+	std::unordered_map<std::string, int> m_UniformLocationCache;
 public:
 	Shader(const std::string& filepath);
 	~Shader();
@@ -25,7 +25,7 @@ public:
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 private:
 	unsigned int CompileShader(unsigned int type, const std::string& source);
-	unsigned int GetUniformLocation(const std::string& name);
+	int GetUniformLocation(const std::string& name);
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
 	ShaderProgramSource ParseShader(const std::string& filepath);
 };
